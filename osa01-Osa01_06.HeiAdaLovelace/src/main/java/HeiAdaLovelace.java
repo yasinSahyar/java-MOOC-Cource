5579bf839976a077a660edeaaa5b4a5efbd93527
800029348 <800029348@OMNIAS-8CXHV93.omnia.lan>
2022-08-26 10:26:32 +0300

    my java homework-yasin

---
+++ osa01-Osa01_06.HeiAdaLovelace/src/main/java/HeiAdaLovelace.java
@@ -0,0 +1,8 @@
+public class HeiAdaLovelace {
+ 
+    public static void main(String[] args) {
+       // String nimi = "Ada Lovelace";
+        String nimi = "Hei" + " Ada Lovelace!";
+        System.out.println(nimi);
+    }
+}
